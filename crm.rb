8ad142bf02d80a844c565bb5dbaa255f78f13d0e
Akshit Sahani <akshit_sahani@hotmail.com>
2017-01-25 16:18:56 -0500

    added a new request to handle editing contact info

--- crm.rb
+++ crm.rb
@@ -16,7 +16,11 @@ end
 
 get '/contacts/:id' do
   @contact = Contact.find(params[:id].to_i)
-  erb :show_contact
+  if @contact
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
 end
 
 get '/contacts' do
@@ -30,4 +34,13 @@ end
 post '/contacts' do
   Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
   redirect to('/contacts')
+end
+
+get '/contacts/:id/edit' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
 end
