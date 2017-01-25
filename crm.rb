90d0ff43b9c6f22a7b8557c6f64792bea5ebb030
Akshit Sahani <akshit_sahani@hotmail.com>
2017-01-25 15:38:18 -0500

    added a new page for a single contact

--- crm.rb
+++ crm.rb
@@ -4,14 +4,19 @@
 require_relative 'contact'
 require 'sinatra'
 
-#fake data
-# Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
+# fake data
+Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
 # Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
 # Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
 
 get '/' do
   @crm_app_name = "Sahani's CRM"
-  erb :index
+   erb :index
+end
+
+get '/contacts/1' do
+  @contact = Contact.find(1)
+  erb :show_contact
 end
 
 get '/contacts' do
